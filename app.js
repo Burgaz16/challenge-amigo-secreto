4fc009ce42d325a33042951795806563a389e325
Burgaz16 <internauta8509@gmail.com>
2025-09-13 17:59:50 -0300

    Crea la lista amigos y la funcion agregarAmigo app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Declaración de variables y listas
+
+let amigos = [];
+
+
+
+//Funciones
+
+function agregarAmigo(){
+    let capturaAmigos = document.getElementById('amigo').value;
+
+    if (capturaAmigos === "") {
+        alert('Por favor, inserte un nombre.');
+    }else {
+        amigos.push(capturaAmigos);
+        //Limpiar input
+        document.querySelector('#amigo').value = '';
+    }
+    
+    return;
+    
+}
+
+
